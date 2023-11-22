34094a503f969156dfa400416e550e100c66e5ea
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 10:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=1;i<str.length-1;i++){
+		if(str.charAt(i)!=str.charAt(i+1) && str.charAt(i)!=str.charAt(i-1)){
+			return str.charAt(i);
+		}else{
+			continue;
+		}
+	}
+	return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
