3b1e1fbfe083052da9a34905829d53f914a83c1f
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 10:18:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+
 function firstNonRepeatedChar(str) {
  // Write your code here
-	for(let i=1;i<str.length-1;i++){
-		if(str.charAt(i)!=str.charAt(i+1) && str.charAt(i)!=str.charAt(i-1)){
-			return str.charAt(i);
-		}else{
-			continue;
-		}
-	}
-	return null;
+	   const charCount = new Map();
+
+    // Iterate through the string to populate the frequency map
+    for (const char of str) {
+        charCount.set(char, (charCount.get(char) || 0) + 1);
+    }
+
+    // Find the first non-repeated character by iterating through the string again
+    for (const char of str) {
+        if (charCount.get(char) === 1) {
+            return char;
+        }
+    }
+
+	
+    // If no non-repeated character is found
+    return null;
 }
+
+
 
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
